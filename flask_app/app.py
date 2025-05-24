0ef0f69edf19451653a232e8d241cf3191bc3b88
seharkansal <seharkansal@gmail.com>
2025-05-24 18:49:39 -0400

    added test

--- flask_app/app.py
+++ flask_app/app.py
@@ -76,19 +76,19 @@ def normalize_text(text):
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
+dagshub_url = "https://dagshub.com"
+repo_owner = "seharkansal"
+repo_name = "MLOPS-CAPSTONE-PROJECT"
 # Set up MLflow tracking URI
-mlflow.set_tracking_uri('https://dagshub.com/seharkansal/MLOPS-CAPSTONE-PROJECT.mlflow')
-dagshub.init(repo_owner='seharkansal', repo_name='MLOPS-CAPSTONE-PROJECT', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/seharkansal/MLOPS-CAPSTONE-PROJECT.mlflow')
+# dagshub.init(repo_owner='seharkansal', repo_name='MLOPS-CAPSTONE-PROJECT', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 
